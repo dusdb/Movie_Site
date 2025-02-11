73cbef5742518511c84378cd339c56a6d04c1bd7
dusdb <yunwoo21s@naver.com>
2025-02-11 15:39:39 +0900

    commit 2

---
+++ react-for-beginners/src/App.js
@@ -0,0 +1,14 @@
+import {BrowserRouter as Router, Routes, Route } from "react-router";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
+function App() {
+  return <Router>
+    <Routes>
+      <Route path="/movie/:id" element={<Detail/>}/>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+  </Router>;
+}
+
+export default App;
